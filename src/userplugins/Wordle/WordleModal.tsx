547af6870bb9ac8de7ee15f07b03958a58cdb69a
equationslayer12 <roy0708usa@gmail.com>
2024-04-07 00:57:06 +0300

    push the button

--- src/userplugins/Wordle/WordleModal.tsx
+++ src/userplugins/Wordle/WordleModal.tsx
@@ -1,9 +1,15 @@
 import { closeModal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, openModal } from "@utils/modal";
+import { Button, Switch } from "@webpack/common";
 
 export function WordleModal({ rootProps }: {rootProps: ModalProps;}) {
+    console.log(rootProps);
     return (
         <ModalRoot {...rootProps}>
-            <h1>Hellooo</h1>
+            <ModalHeader justify="center" className="">
+                Title
+            </ModalHeader>
+            
+            <Button>Button</Button>
         </ModalRoot>
     )
 }
